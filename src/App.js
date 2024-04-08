3e933700ff85b1ae959f472f818a6d2705ddf309
dev-jay-jay <dev-jay-jay>
2024-04-08 09:59:46 +0100

    footer done few changes remaining working on hero section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PageFooter from "./components/pages/PageFooter";
+import PageHero from "./components/pages/PageHero";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="PageWrapper">
+        <PageHero />
+        <PageFooter />
+      </div>
     </div>
   );
 }
---
+++ src/components/organisms/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <div className='FooterWrapper'>
+      <div>All Right Reserved <span>@</span><strong>dev-jay-jay</strong> </div>
+    </div>
+  )
+}
+
+export default Footer;
