bb51dcf6b98344db37b1254e841379777ac27274
dev-jay-jay <dev-jay-jay>
2024-04-15 15:52:49 +0100

    project logic done real content filling remaining

--- src/components/pages/PageProject.js
+++ src/components/pages/PageProject.js
@@ -1,12 +1,41 @@
 import "./style.css";
 import React from "react";
 import ProjectCard from "../molecules/ProjectCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 2,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const PageProject = () => {
   return (
     <div className="ProjectPageWrapper">
       <h5>Projects And Colaborations</h5>
-      <div className="ProjectPageContainer">
+      <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        autoPlaySpeed={5000}
+        transitionDuration={500}
+        infinite
+        className="ProjectPageContainer"
+      >
         {PageContent.map((item, index) => (
           <ProjectCard
             key={index}
@@ -16,7 +45,7 @@ const PageProject = () => {
             technologies={item.technologies}
           />
         ))}
-      </div>
+      </Carousel>
     </div>
   );
 };
@@ -25,33 +54,104 @@ export default PageProject;
 
 const PageContent = [
   {
-    image: "/image/check.png",
+    image: [
+      {
+        img: "/image/hero1.png",
+      },
+      {
+        img: "/image/jayjay.jpeg",
+      },
+      {
+        img: "/image/check.png",
+      },
+    ],
     name: "Schovela Version 1 & 2",
-    content: "I'm John Jonathan Gaiya, a cool-headed Software Developer and avid learner. I'm passionate about growth, both for myself and others. Let's code and grow together!",
+    content:
+      "I'm John Jonathan Gaiya, a cool-headed Software Developer and avid learner. I'm passionate about growth, both for myself and others. Let's code and grow together!",
     technologies: [
       {
-        icon : "</>",
-        text : "One",
+        icon: "</>",
+        text: "One",
       },
       {
-        icon : "#",
-        text : "Two",
+        icon: "#",
+        text: "Two",
       },
       {
-        icon : "</>",
-        text : "Three",
+        icon: "</>",
+        text: "check",
       },
       {
-        icon : "</>",
-        text : "One",
+        icon: "</>",
+        text: "One",
       },
       {
-        icon : "#",
-        text : "Two",
+        icon: "#",
+        text: "Two",
       },
       {
-        icon : "</>",
-        text : "Three",
+        icon: "</>",
+        text: "Three",
+      },
+    ],
+  },
+  {
+    image: [
+      {
+        img: "/image/hero1.png",
+      },
+      {
+        img: "/image/jayjay.png",
+      },
+      {
+        img: "/image/check.png",
+      },
+    ],
+    name: "Schovela Version 1 & 2",
+    content:
+      "I'm John Jonathan Gaiya, a cool-headed Software Developer and avid learner. I'm passionate about growth, both for myself and others. Let's code and grow together!",
+    technologies: [
+      {
+        icon: "</>",
+        text: "Next",
+      },
+      {
+        icon: "#",
+        text: "Styled Component",
+      },
+      {
+        icon: "</>",
+        text: "Css",
+      },
+    ],
+  },
+  {
+    image: [
+      {
+        img: "/image/hero1.png",
+      },
+      {
+        img: "/image/jayjay.png",
+      },
+      {
+        img: "/image/check.png",
+      },
+    ],
+    name: "Schovela Version 1 & 2",
+    content:
+      "I'm John Jonathan Gaiya, a cool-headed Software Developer and avid learner. I'm passionate about growth, both for myself and others. Let's code and grow together!",
+    technologies: [
+      {
+        icon: "</>",
+        text: "Next",
+      },
+      {
+        icon: "#",
+        text: "Styled Component",
+      },
+      {
+        icon: "</>",
+        text: "Css",
       },
     ],
   },
