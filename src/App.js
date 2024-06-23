410952bd816b6a0e704676f3c7a951bd4c39c812
dev-jay-jay <johngratia2@gmail.com>
2024-06-23 21:42:42 +0100

    section sub headers set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import About from "./components/About"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div>
      <NavBar />
      <Hero />
+     <About />
     </div>
   );
 }
--- src/components/Atoms.js
+++ src/components/Atoms.js
@@ -1,4 +1,22 @@
 
-function name(params) {
-    
+function SectionsSubHeaders({text}) {
+    return(
+        <div>
+            <div className="flex flex-row items-center w-[80%] mx-auto gap-4">
+                <p className="text-5xl text-green-500">{text}</p>
+                <span className="bg-green-500 w-[70%] h-[2.5px] rounded-full mt-2"></span>
+            </div>
+        </div>
+    );
 }
+
+function Skills() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+const Atoms = { SectionsSubHeaders, Skills };
+export default Atoms;
